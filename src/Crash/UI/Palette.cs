f104a46ba0d540b5768a0c131b378f0f9c36a943
Clicketyclackety <csykes@crsh.cloud>
2023-08-24 18:56:35 -0700

    Added UI Colour Palette

---
+++ src/Crash/UI/Palette.cs
@@ -0,0 +1,22 @@
+﻿using Eto;
+using Eto.Drawing;
+
+namespace Crash.UI
+{
+	public static class Palette
+	{
+		// Text & Highlights
+		public static Color TextColour => Color.FromArgb(29, 29, 27);
+		public static Color White => Color.FromArgb(255,255,255);
+		public static Color Transparent => Color.FromArgb(0, 0, 0, 0);
+
+		public static Color Grey => Color.FromArgb(125, 125, 125);
+		public static Color SubtleGrey => Color.FromArgb(200, 200, 200);
+
+		// Crash Colours
+		public static Color NavyBlue => Color.FromArgb(29, 48, 146);
+		public static Color SeaGreen => Color.FromArgb(129, 226, 200);
+
+	}
+
+}
