461c500413aa169887a9c7f19928bf464f4d6d25
Clicketyclackety <csykes@crsh.cloud>
2024-01-13 08:12:03 -0800

    Build fixes

--- src/Crash.Common/Collections/IdleAction.cs
+++ src/Crash.Common/Collections/IdleAction.cs
@@ -25,7 +25,7 @@ namespace Crash.Events
 		}
 
 		/// <summary>True if successfully invoked</summary>
-		private bool Invoked { get; set; }
+		internal bool Invoked { get; set; }
 
 
 		public void Dispose()
